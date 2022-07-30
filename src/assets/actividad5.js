daef5297a5bd28ddf7817e8507cde2537f06610c
hamiltonguerrag <105325997+hamiltonguerrag@users.noreply.github.com>
2022-07-29 19:19:16 -0500

    Update actividad5.js

--- src/assets/actividad5.js
+++ src/assets/actividad5.js
@@ -1,44 +1,44 @@
 console.log('En linea:actividad 5')
 
 
-let  tipoHuevo  =  parseInt ( prompt ( `¿Cúal es el tipo de huevos que quieres comprar?: Elije 1 para llevar huevos tipo 'A' o elije 2 para llevar huevos tipo 'AA'` ) )
+let  huevo  =  parseInt ( prompt ( `¿Cúal es el tipo de huevos que quieres comprar?: Elije 1 para llevar huevos tipo 'A' o elije 2 para llevar huevos tipo 'AA'` ) )
 
-let  huevosAdquiridos  =  parseInt ( prompt ( 'Ingrese la cantidad de huevos que desea llevar' ) )
+let  huevoPorAdquirir  =  parseInt ( prompt ( 'Ingrese la cantidad de huevos que desea llevar' ) )
 
-if  ( tipoHuevo  =  1  &&  huevosAdquiridos  <  99 ) {
-    alert ( `Usted debe pagar una cantidad de ${ ( 220  *  huevosAdquiridos ) } ` )
+if  ( huevo  =  1  &&  huevoPorAdquirir  <  99 ) {
+    alert ( `Usted debe pagar una cantidad de ${ ( 220  *  huevoPorAdquirir ) } ` )
 } 
 más 
-if  ( tipoHuevo  =  1  &&  huevosAdquiridos  >  100  &&  huevosAdquiridos  <  200 ) {
-    alert ( `Usted debe pagar una cantidad de ${ ( 220  *  huevosAdquiridos )  /  0.05 } ` )
+if  ( huevo  =  1  &&  huevoPorAdquirir  >  100  &&  huevoPorAdquirir  <  200 ) {
+    alert ( `Usted debe pagar una cantidad de ${ ( 220  *  huevoPorAdquirir )  /  0.05 } ` )
 } 
 más
-if  ( tipoHuevo  =  1  &&  huevosAdquiridos  >  101  &&  huevosAdquiridos  <  201 ) {
-    alert ( `Usted debe pagar una cantidad de ${ ( 220  *  huevosAdquiridos )  /  0.05 } ` )
+if  ( huevo  =  1  &&  huevoPorAdquirir  >  101  &&  huevoPorAdquirir  <  201 ) {
+    alert ( `Usted debe pagar una cantidad de ${ ( 220  *  huevoPorAdquirir )  /  0.05 } ` )
 }
 más
-if  ( tipoHuevo  =  1  &&  huevosAdquiridos  >  221  &&  huevosAdquiridos  <  301 ) {
-    alert ( `Usted debe pagar una cantidad de ${ ( 220  *  huevosAdquiridos )  /  0.08 } ` )
+if  ( huevo  =  1  &&  huevoPorAdquirir  >  221  &&  huevoPorAdquirir  <  301 ) {
+    alert ( `Usted debe pagar una cantidad de ${ ( 220  *  huevoPorAdquirir)  /  0.08 } ` )
 }
 más
-if  ( tipoHuevo  =  1  &&  huevosAdquiridos  >  301 ) {
-    alert ( `Usted debe pagar una cantidad de ${ ( 220  *  huevosAdquiridos )  /  0.1 } ` )
+if  ( huevo  =  1  &&  huevoPorAdquirir  >  301 ) {
+    alert ( `Usted debe pagar una cantidad de ${ ( 220  *  huevoPorAdquirir )  /  0.1 } ` )
 }
 más
-if  ( tipoHuevo  =  2  &&  huevosAdquiridos  <  101 ) {
-    alert ( `Usted debe pagar una cantidad de ${ ( 220  *  huevosAdquiridos ) } ` )
+if  ( huevo  =  2  &&  huevoPorAdquirir  <  101 ) {
+    alert ( `Usted debe pagar una cantidad de ${ ( 220  *  huevoPorAdquirir ) } ` )
 }
 más 
-if ( tipoHuevo  =  2  &&  tipoHuevo  >  100  &&  huevosAdquiridos  <  201 ) {
-    alert ( `El total a pagar es ${ ( 250  *  huevosAdquiridos ) /  0.05 } ` )
+if ( huevo  =  2  &&  huevoPorAdquirir  >  100  &&  huevoPorAdquirir  <  201 ) {
+    alert ( `El total a pagar es ${ ( 250  *  huevoPorAdquirir ) /  0.05 } ` )
 }
 más 
-if ( tipoHuevo  =  2  &&  huevosAdquiridos >  201  &&  huevosAdquiridos  <  301 ) {
-    alert ( `El total a pagar es ${ ( 250  *  huevosAdquiridos ) /  0.08 } ` )
+if ( huevo  =  2  &&  huevoPorAdquirir >  201  &&  huevoPorAdquirir  <  301 ) {
+    alert ( `El total a pagar es ${ ( 250  *  huevoPorAdquirir ) /  0.08 } ` )
 }
 más 
-if ( tipoHuevo  =  2  &&  huevosAdquiridos  >=  301 ) {
-    alert ( `El total a pagar es ${ ( 250  *  huevosAdquiridos ) /  0.10 } ` )
+if ( huevo =  2  &&  huevoPorAdquirir  >=  301 ) {
+    alert ( `El total a pagar es ${ ( 250  *  huevoPorAdquirir) /  0.10 } ` )
 } 
 más 
 alert ( `Para validar tu compra, debes elegir 1 o 2 para poder hacer tu compra, de lo contrario no sera realizado`)
